9a29843bd4f4912450fe91f97f5cb887b53ecd34
Murilo Bagodi <murilosbagodi@hotmail.com>
2022-03-02 18:49:58 -0300

    nodemailer fix

--- index.js
+++ index.js
@@ -25,38 +25,40 @@ const pass = "$$%%123+-/money";
 
 app.post("/env/renata", (req, res) => {
   const transporter1 = nodemailer.createTransport({
-    host: "SMTP.office365.com",
-    port: "25",
+    host: "smtp.office365.com",
+    port: "587",
     auth: { user: user, pass: pass },
   });
 
   var email = req.body.mailR;
   var assunto = req.body.assuntoR;
   var textEmail = req.body.txtAreaR;
-
-  transporter1
-    .sendMail({
-      from: email,
-      to: "rgs.rac@gmail.com",
-      replyTo: "jccs.rac@gmail.com",
-      subject: assunto,
-      text: textEmail,
-    })
-    .then(() => {
-      res.send("Email enviado com sucesso");
-    })
-    .catch((error) => {
-      res.send(
-        "Erro, Tente novamente mais tarde ou troque o navegador utilizado (Recomenda-se o Google-Chrome)"
-      );
-    });
+  async function envEmail1() {
+    transporter1
+      .sendMail({
+        from: email,
+        to: "rgs.rac@gmail.com",
+        replyTo: "jccs.rac@gmail.com",
+        subject: assunto,
+        text: textEmail,
+      })
+      .then(() => {
+        res.send("Email enviado com sucesso");
+      })
+      .catch((error) => {
+        res.send(
+          "Erro, Tente novamente mais tarde ou troque o navegador utilizado (Recomenda-se o Google-Chrome)"
+        );
+      });
+  }
+  envEmail1().catch(console.error);
 });
 
 app.post("/env/joao", (req, res) => {
   async function envEmail() {
     const transporter = nodemailer.createTransport({
-      host: "SMTP.office365.com",
-      port: "25",
+      host: "smtp.office365.com",
+      port: "587",
       auth: { user: user, pass: pass },
     });
 
@@ -80,7 +82,7 @@ app.post("/env/joao", (req, res) => {
       });
   }
 
-  envEmail();
+  envEmail().catch(console.error);
 });
 
 app.listen(port, () => {
